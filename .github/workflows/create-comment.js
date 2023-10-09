fdd9d059964cf45982c7df480dcbfd50e1d4608b
Leiser Mahu <leiser.mahu@gmail.com>
2023-10-09 18:26:10 -0300

    Update create-comment.js

--- .github/workflows/create-comment.js
+++ .github/workflows/create-comment.js
@@ -1,11 +1,7 @@
 const { context, github } = require('@actions/github');
 
 async function run() {
-  const labels = context.payload.pull_request.labels.map(label => label.name);
-  const defaultLabel = labels.find(label => label === 'patch'); // Use 'patch' as the default label
-
-  const body = `Please review the code carefully before merging and check the following box if the correct type of version is going to be merged\n\n- [ ] This PR means a new version of the package ("${defaultLabel}")`;
-
+  const body = `Please review the code carefully before merging and check the following box if the correct type of version is going to be merged\n\n- [ ] This PR means a new version of the package (patch)`;
   await github.issues.createComment({
     issue_number: context.payload.pull_request.number,
     owner: context.repo.owner,
