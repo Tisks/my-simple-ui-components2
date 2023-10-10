9c6b33cf92ad30893edcbd40e7df36fb3ad6b109
Tisks <leiser.mahu@usach.cl>
2023-10-10 11:27:07 -0300

    chore: lets call the api

--- generate-changelog.js
+++ generate-changelog.js
@@ -1,30 +1,10 @@
-const { spawnSync } = require('child_process');
+const { execSync } = require('child_process');
 const fs = require('fs');
 
 const priorVersion = process.argv[2];
 const newVersion = process.argv[3];
 
-const args = [
-  'conventional-changelog-cli',
-  '-p',
-  'react',
-  '-r',
-  '0',
-  '-t',
-  priorVersion,
-  '-n',
-  newVersion,
-];
-
-const result = spawnSync('npx', args, { stdio: 'pipe', encoding: 'utf-8' });
-
-if (result.error) {
-  console.error(result.error);
-  process.exit(1);
-}
-
-const changelog = result.stdout;
+const changelog = execSync(`npx conventional-changelog-cli -p angular -r 0 -t ${priorVersion} -n ${newVersion}`).toString();
 
 fs.writeFileSync('CHANGELOG.md', changelog);
-console.log(changelog);
 console.log('Changelog generated and saved.');
