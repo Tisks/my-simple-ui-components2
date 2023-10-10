884c61833ffb00d126635fe3034eecc26312f4df
Tisks <leiser.mahu@usach.cl>
2023-10-10 04:25:00 -0300

    fix

--- generate-changelog.js
+++ generate-changelog.js
@@ -1,11 +1,30 @@
-const { execSync } = require('child_process');
+const { spawnSync } = require('child_process');
 const fs = require('fs');
 
 const priorVersion = process.argv[2];
 const newVersion = process.argv[3];
 
-const changelog = execSync(`npx conventional-changelog-cli -p react -r 0 -t ${priorVersion} -n ${newVersion}`).toString();
+const args = [
+  'conventional-changelog-cli',
+  '-p',
+  'react',
+  '-r',
+  '0',
+  '-t',
+  priorVersion,
+  '-n',
+  newVersion,
+];
+
+const result = spawnSync('npx', args, { stdio: 'pipe', encoding: 'utf-8' });
+
+if (result.error) {
+  console.error(result.error);
+  process.exit(1);
+}
+
+const changelog = result.stdout;
 
 fs.writeFileSync('CHANGELOG.md', changelog);
-console.log(changelog)
+console.log(changelog);
 console.log('Changelog generated and saved.');
