2be464bae710922d0a1f33e18a952066452812b9
Leiser Mahu <leiser.mahu@gmail.com>
2023-10-10 01:50:43 -0300

    Create get-version.js

---
+++ .github/workflows/get-version.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+try {
+  const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+  const version = packageJson.version;
+  console.log(version);
+} catch (error) {
+  console.error('Error reading or parsing package.json:', error);
+  process.exit(1);
+}
