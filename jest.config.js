5a6fc47716a9acf55395e2401ac4b7c547406723
Tisks <leiser.mahu@usach.cl>
2023-10-11 23:15:13 -0300

    chore: adding names

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,12 @@ module.exports = {
   },
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 75,
+      functions: 80,
+      lines: 90,
+    },
+  },
 };
