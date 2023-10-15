1dc2355f43887b8d5e5e613b7512dc2229dee359
Tisks <leiser.mahu@usach.cl>
2023-10-15 03:54:22 +0000

    Deploying to gh-pages from @ Tisks/my-simple-ui-components2@0d1d5fc7909d53d26132a8ca9135a403de00f29e 🚀

---
+++ assets/preview-05562d8f.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-e19f6967.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-5fce8964.js"),["./DocsRenderer-3PUGWF3O-5fce8964.js","./iframe-e19f6967.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-38063e1e.js","./index-ffc7e5ff.js","./index-19ecb609.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-05562d8f.js.map
